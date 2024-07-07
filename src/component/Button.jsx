13d31427d1119853a53c4aab7432356b2deb51a0
connect.abdulbasit <contact.abdulbasit.cs@gmail.com>
2024-07-07 22:04:24 +0500

    HEADER COMPONENT DONE

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
-const Button = ({ className, href, onclick, px, white, children }) => {
+const Button = ({ className, href, onClick, px, white, children }) => {
   const classes = `button relative infline-flex items-center justify-center h-11 transitions-colors ${
     className || ""
   } ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"}`;
   const spanClasses = "relative z-10";
   const RenderButton = () => (
-    <button className={classes} onclick={onclick}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,16 +2,44 @@ import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
+import { HamburgerMenu } from "../component/design/Header";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { useEffect, useState } from "react";
 const Header = () => {
   const location = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(true);
+  const ToggleOpenNavigation = () => {
+    const navigation = !openNavigation;
+    setOpenNavigation(navigation);
+    console.log(openNavigation);
+  };
+  const handleClick = () => {
+    setOpenNavigation(false);
+  };
+  useEffect(() => {
+    if (openNavigation) {
+      disablePageScroll();
+    } else {
+      enablePageScroll();
+    }
+  }, [openNavigation]);
   return (
     <>
-      <div className="fixed top-0 left-0 w-full z-50 backdrop-blur-sm bg-n-8/90 border-b border-n-6  lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div
+        className={`fixed top-0 left-0 w-full z-50   border-b border-n-6  lg:bg-n-8/90 lg:backdrop-blur-sm ${
+          openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+        }`}
+      >
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 ax-lg:py-4">
           <a className="block w-[12rem] lg:mr-8" href="#hero">
             <img src={brainwave} width={190} height={40} alt="Brainwave" />
           </a>
-          <nav className="hidden fixed top-[5rem] right-0 left-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <nav
+            className={`${
+              openNavigation ? "flex" : "hidden"
+            } fixed top-[5rem] right-0 left-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          >
             <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
               {navigation.map((item) => (
                 <a
@@ -24,11 +52,13 @@ const Header = () => {
                       ? "z-2 lg:text-n-1"
                       : "lg:text-n-1/50"
                   } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                  onClick={handleClick}
                 >
                   {item.title}
                 </a>
               ))}
             </div>
+            <HamburgerMenu />
           </nav>
           <a
             href="#signup"
@@ -38,6 +68,13 @@ const Header = () => {
           </a>
           <Button className={"hidden lg:flex"} href="#login">
             Sign In
+          </Button>
+          <Button
+            className={`ml-auto lg:hidden`}
+            px="px-3"
+            onClick={ToggleOpenNavigation}
+          >
+            <MenuSvg openNavigation={openNavigation} />
           </Button>
         </div>
       </div>
